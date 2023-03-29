0d85e153ab42511c63476b3b880ece7efda59731
Bek <nematjanov95@gmail.com>
2023-03-30 01:26:52 +0500

    fixing bugs

---
+++ lib/presentation/components/custom_textfromfiled.dart
@@ -0,0 +1,123 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:provider/provider.dart';
+
+import '../../controller/auth_controller.dart';
+import '../style/style.dart';
+
+class CustomTextFrom extends StatelessWidget {
+  final String hintext;
+  final Widget? suffixicon;
+  final Icon? perfixicon;
+  final TextEditingController? controller;
+  final TextInputType keyboardType;
+  final String? label;
+  final ValueChanged<String>? onChange;
+  final String? obscuringCharacter;
+  final bool isObscure;
+  final Color? fillColor;
+  final bool? filled;
+  final FormFieldValidator? validator;
+  final FocusNode? node;
+  final TextStyle? style;
+  final List<TextInputFormatter>? format;
+
+  const CustomTextFrom(
+      {Key? key,
+      required this.hintext,
+      this.keyboardType = TextInputType.text,
+      this.suffixicon,
+      this.label,
+      this.onChange,
+      this.perfixicon,
+      this.controller,
+      this.obscuringCharacter,
+      required this.isObscure,
+      this.fillColor,
+      this.filled,
+      this.validator,
+      this.node,
+      this.style,
+      this.format})
+      : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      height: 60,
+      child: TextFormField(
+        inputFormatters: format,
+        onChanged: onChange,
+        style: style,
+        validator: validator,
+        focusNode: node,
+        keyboardType: keyboardType,
+        obscureText:
+            isObscure ? (context.watch<AuthController>().isVisibility) : false,
+        controller: controller,
+        decoration: InputDecoration(
+          fillColor: fillColor,
+          filled: filled,
+          suffixIcon: suffixicon ??
+              (isObscure
+                  ? IconButton(
+                      onPressed: () {
+                        context.read<AuthController>().onChange();
+                      },
+                      icon: context.watch<AuthController>().isVisibility
+                          ? const Icon(Icons.visibility)
+                          : const Icon(Icons.visibility_off),
+                    )
+                  : const SizedBox.shrink()),
+          prefix: perfixicon,
+          // suffix: suffixicon,
+          label: Text('$label'),
+          labelStyle:
+              Style.textStyleRegular2(textColor: Style.greyColor90, size: 15),
+          hintText: hintext,
+          // contentPadding:
+          //     const EdgeInsets.only(left: 24, right: 80, top: 12, bottom: 12),
+          errorBorder: const OutlineInputBorder(
+            borderSide: BorderSide(
+              color: Colors.red,
+            ),
+            borderRadius: BorderRadius.all(
+              Radius.circular(12),
+            ),
+          ),
+
+          focusedErrorBorder: const OutlineInputBorder(
+            borderSide: BorderSide(
+              color: Colors.red,
+            ),
+            borderRadius: BorderRadius.all(
+              Radius.circular(12),
+            ),
+          ),
+          border: const OutlineInputBorder(
+            borderSide: BorderSide(
+              color: Color(0xffAFB0B6),
+            ),
+            borderRadius: BorderRadius.all(
+              Radius.circular(12),
+            ),
+          ),
+          enabledBorder: const OutlineInputBorder(
+            borderSide: BorderSide(
+              color: Color(0xffAFB0B6),
+            ),
+            borderRadius: BorderRadius.all(
+              Radius.circular(12),
+            ),
+          ),
+          focusedBorder: const OutlineInputBorder(
+            borderSide: BorderSide(color: Style.primaryColor),
+            borderRadius: BorderRadius.all(
+              Radius.circular(12),
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
